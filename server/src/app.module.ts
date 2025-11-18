00a26519c831f1c13d513733b19452f411e2c8fb
Diogo <diogodelorenzipinheiro@gmail.com>
2025-11-18 11:53:16 -0300

    setup inicial: instalacoes dos pacotes, criação das pastas client e server e preparacao do banco de dados.

---
+++ server/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    // Configuração do Banco
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'projeto_db',
+      entities: [], // Aqui entrarão suas entidades depois
+      synchronize: true, // Apenas para DEV/TESTE (cria tabelas sozinho)
+    }),
+    // Configuração do GraphQL
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+})
+export class AppModule {}
